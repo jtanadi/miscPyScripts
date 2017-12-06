bd697b736660f91108e9578c6aeabca1c1e8fa3e
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-12-06 10:33:33 -0500

    move tests to different repo

--- writeFileTest.py
+++
@@ -1,5 +0,0 @@
-myDict = {"TH_ex_01":"Hello how are you", "TH_ex_02":"Buona serra!", "TH_ex03":"Good afternoon!"}
-
-for key in myDict:
-    with open(key + ".txt", "w") as f:
-        f.write(myDict[key])
