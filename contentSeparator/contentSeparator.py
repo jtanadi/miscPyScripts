a2dd03ef30c9f3586306684f397ef6bd96bb73b9
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-12-07 17:30:37 -0500

    update folder org for TH scripts

--- contentSeparator/contentSeparator.py
+++ contentSeparator/contentSeparator.py
@@ -1,9 +1,9 @@
 import os
 
 # Remember to change directory
-os.chdir("/Volumes/3Projects/OVMM-OhioVetMem/02_CONTENT/Exhibit Script_FINAL/Thematic Displays/z-JT/")
+os.chdir("/Users/jesentanadi/Desktop/Ohio Test/Script/")
 
-with open("_TL_NC.txt", 'rU') as readFile: #.txt file
+with open("_TH.txt", 'rU') as readFile: #.txt file
     inputTextList = readFile.readlines() #Returns a list
 
 inputTextList = [text for text in inputTextList if text != "\n"]
@@ -11,24 +11,37 @@ inputTextList = [text for text in inputTextList if text != "\n"]
 codeIndex = [index for index, entry in enumerate(inputTextList) if "_" in entry]
 codeIndex.append(len(inputTextList))
 
-contentDict = {inputTextList[codeIndex[i]].replace("\n", "") : \
+contentDict = {inputTextList[codeIndex[i]].replace("\n", "").split(" ")[0] : \
                inputTextList[codeIndex[i]+1:codeIndex[i+1]] \
                for i in range(len(codeIndex)-1)}
 
 def makeFolderName(fileName):
-    gallery, exhibit, story = fileName.split("_")
+    exhibit, topic, story = fileName.split("_")
 
-    if gallery == "TL":
-        if any(n in story for n in ["pt", "st", "ti"]):
-            return "{}/{}_{}/01-National Chronology".format(gallery, gallery, exhibit)
+    if exhibit == "TH":
+        if "ip" in story:
+            category = "0-Intro"
 
-        elif "tt" in story:
-            return "{}/{}_{}/02-Veterans Stories".format(gallery, gallery, exhibit)
+        elif "pt" in story:
+            category = "1-Primary"
 
-        elif "dl" in story:
-            return "{}/{}_{}/03-Dateline".format(gallery, gallery, exhibit)
+        elif "st" in story:
+            category = "2-Secondary"
 
-    return "{}/{}_{}".format(gallery, gallery, exhibit)
+        return "{}/{}/".format(exhibit, category)
+
+    """
+    Fix TL version below once folder structure has been decided.
+    """
+    # if gallery == "TL":
+    #     if any(n in story for n in ["pt", "st", "ti"]):
+    #         return "{}/{}_{}/01-National Chronology".format(gallery, gallery, exhibit)
+
+    #     elif "tt" in story:
+    #         return "{}/{}_{}/02-Veterans Stories".format(gallery, gallery, exhibit)
+
+    #     elif "dl" in story:
+    #         return "{}/{}_{}/03-Dateline".format(gallery, gallery, exhibit)
 
 
 def makeFolder(folder):
@@ -39,11 +52,20 @@ def makeFolder(folder):
 
 
 for key in contentDict:
+
+    title = contentDict[key][0].replace("\n", "")
+    body = contentDict[key][1:]
+
     pathName = makeFolderName(key)
     makeFolder(str(pathName))
 
-    filePath = os.path.join(pathName, key + ".txt")
+    titlePath = os.path.join(pathName, key.upper() + "-T.txt")
+    bodyPath = os.path.join(pathName, key.upper() + "-B.txt")
 
-    with open(filePath, "w") as writeFile:
-        for item in contentDict[key]:
-            writeFile.write(item)
+    with open(titlePath, "w") as writeTitleFile:
+        for item in title:
+            writeTitleFile.write(item)
+
+    with open(bodyPath, "w") as writeBodyFile:
+        for item in body:
+            writeBodyFile.write(item)
